d3f972a19c59dbf42192ebdb8c9f689d9dfbf21d
Sampong-Starluck <sampong.lim@gmail.com>
2022-06-07 15:02:04 +0700

    chapter 7

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,5 +7,23 @@ export class AppController {
 
   @Get("/")
   @Render('index')
-  index(){}
+  index() {
+    return {
+      title: "Home Page - Online Store"
+    };
+  }
+
+  @Get("/about")
+  @Render('about')
+  about() {
+    let viewData = [];
+    viewData["decription"] = "This is an about page ....";
+    viewData["author"] = "Developed by: Sampong";
+    let data1 = 'About us - Online Store';
+    return {
+      title: data1,
+      subtitle: "About Us",
+      viewData: viewData
+    }
+  }
 }
