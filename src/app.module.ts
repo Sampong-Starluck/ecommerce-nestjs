32269f3b31165ab14c3374559992aaec9aeafc41
Sampong-Starluck <sampong.lim@gmail.com>
2022-06-08 16:36:42 +0700

    add env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,20 @@ import { ProductsController } from './products.controller';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { env } from 'process';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 // import { AppService } from './app.service';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'sampong3788',
-      database: 'nest_online_store',
+      host: env.MYSQL_HOST,
+      port: parseInt(env.MYSQL_PORT),
+      username: env.MYSQL_USERNAME,
+      password: env.MYSQL_PASSWORD,
+      database: env.MYSQL_DATABASE,
       entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
