f92c9ca2025efa39221ac8e9f36e76c63c4b2a23
Sampong-Starluck <sampong.lim@gmail.com>
2022-06-08 16:14:53 +0700

    Chapter 12

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { ProductsController } from './products.controller';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 // import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'sampong3788',
+      database: 'nest_online_store',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [ProductsController, AppController],
   // providers: [AppService],
 })
