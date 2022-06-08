494001907bb5f2e844dc7d878899f5525166e815
Sampong-Starluck <sampong.lim@gmail.com>
2022-06-08 16:59:34 +0700

    add module chapter 13

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { ProductsController } from './products.controller';
+import { ProductsController } from './products/products.controller';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -6,6 +6,7 @@ import { env } from 'process';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
 // import { AppService } from './app.service';
+// import { ProductsModule } from './products.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ require('dotenv').config();
       entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
+    // ProductsModule,
   ],
   controllers: [ProductsController, AppController],
   // providers: [AppService],
